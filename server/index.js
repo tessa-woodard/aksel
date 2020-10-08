2c0afe11e430f91277cd1b1813ecbbebccf186c2
Tessa Woodard <tessawoodard@outlook.com>
2020-10-07 21:16:39 -0400

    test

--- server/index.js
+++ server/index.js
@@ -33,6 +33,30 @@ io.on('connection', socket => {
     })
 })
 
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'tessawoodard.me@gmail.com',
+        pass: 'passWord752!@'
+    }
+});
+
+const mailOptions = {
+    from: 'sender@email.com', // sender address
+    to: 'to@email.com', // list of receivers
+    subject: 'Subject of your email', // Subject line
+    html: '<p>Your html here</p>'// plain text body
+};
+
+transporter.sendMail(mailOptions, function (err, info) {
+    if (err)
+        console.log(err)
+    else
+        console.log(info);
+});
+
 
 app.use(express.json())
 
